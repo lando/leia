0f5b06a3dd407c9b41e88e48f8cda8ca32c03d87
Mike Pirog <mike@thinktandem.io>
2018-11-27 00:55:29 -0500

    Update README

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+v0.3.0 - [November 27, 2018](https://github.com/lando/leia/releases/tag/v0.3.0)
+----------------------------
+
+* Just a little placeholder [#1](https://github.com/lando/leia/issues/1)
+
--- test/leia.func.js
+++ test/leia.func.js
@@ -21,10 +21,58 @@ describe('leia', function() {
   // These tests are the main event
   // @todo: It would be nice to eventually get these into mocha after hooks
   // so they run after every test
+  it('clean up previous test', done => {
+    process.chdir(path.resolve(__dirname, '..'));
+    const cli = new CliTest();
+    cli.exec('rm -f test/leia.readme.js').then(res => {
+      if (res.error === null) {
+        done();
+      } else {
+        done(res.error);
+      }
+    });
+  });
+
   it('get the version of leia', done => {
     process.chdir(path.resolve(__dirname, '..'));
     const cli = new CliTest();
-    cli.exec('node ./bin/leia.js --version').then(res => {
+    cli.exec('yarn leia --version').then(res => {
+      if (res.error === null) {
+        done();
+      } else {
+        done(res.error);
+      }
+    });
+  });
+
+  it('generate tests from this readme with some options', done => {
+    process.chdir(path.resolve(__dirname, '..'));
+    const cli = new CliTest();
+    cli.exec('yarn leia README.md test -r 6 -o readme.js').then(res => {
+      if (res.error === null) {
+        done();
+      } else {
+        done(res.error);
+      }
+    });
+  });
+
+  it('validate the new test was created', done => {
+    process.chdir(path.resolve(__dirname, '..'));
+    const cli = new CliTest();
+    cli.exec('cat test/leia.readme.js | grep "id: leia"').then(res => {
+      if (res.error === null) {
+        done();
+      } else {
+        done(res.error);
+      }
+    });
+  });
+
+  it('validate the retry was set correctly', done => {
+    process.chdir(path.resolve(__dirname, '..'));
+    const cli = new CliTest();
+    cli.exec('cat test/leia.readme.js | grep retries | grep 6').then(res => {
       if (res.error === null) {
         done();
       } else {
