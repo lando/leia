8f0ca3f5848afe1bb5df6613245509dede045fd7
codebymikey <9484406+codebymikey@users.noreply.github.com>
2021-05-18 20:42:45 +0100

    Escape special characters

--- test/parse.spec.js
+++ test/parse.spec.js
@@ -7,6 +7,8 @@
 
 const chai = require('chai');
 const path = require('path');
+const vm = require('vm');
+
 chai.should();
 
 const parse = require('./../lib/parse');
@@ -24,6 +26,27 @@ describe('parse', () => {
     const tests2 = parse([path.resolve(__dirname, '..', 'examples', 'basic-example.md')]);
     tests2[0].tests.should.not.have.all.keys('setup', 'cleanup');
     tests2[0].tests.should.have.all.keys('test', 'nope');
+  });
+  it('should escape special characters', () => {
+    const tests = parse([path.resolve(__dirname, '..', 'examples', 'special-characters-example.md')]);
+    tests[0].tests.test.should.be.an('Array').and.not.be.empty;
+    const evalRawString = rawString => {
+      rawString.should.be.a('String');
+      rawString.should.have.lengthOf.above(2);
+      // Starts and ends with single quotes.
+      rawString[0].should.equal(`'`);
+      rawString.charAt(rawString.length - 1).should.equal(`'`);
+      // Attempt to "safely" evaluate the raw string.
+      return vm.runInNewContext(rawString);
+    };
+    // Lodash's lowerCase should strip out special characters.
+    tests[0].tests.test[0].describe[0].should.equal('should process special pec a 1 characters');
+    evalRawString(tests[0].tests.test[0].command).should.equal(`echo '"[]\\/@%+=:,.-'`);
+    evalRawString(tests[0].tests.test[1].command).should.equal(`echo "'\\""`);
+    evalRawString(tests[0].tests.test[2].command).should.equal(`echo "\\t\\n'"`);
+    evalRawString(tests[0].tests.test[3].command).should.equal(`echo lando psql -U postgres database -c "\\dt"`);
+    evalRawString(tests[0].tests.test[4].command).should.equal(`echo '\\\\literal\\\\'`);
+    evalRawString(tests[0].tests.test[5].command).should.equal(`echo "\\dt"`);
   });
   it('should return tests as objects with description and command');
   it('should concatenate multiline test commands with a &');
