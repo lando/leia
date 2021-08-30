51dd01404afce611962bf33f410bfc2df586696a
John Ouellet <john@thinktandem.io>
2021-08-30 10:57:27 -0400

    Adjust generate fun tests to accomidate different os temp dirs

--- test/generate.spec.js
+++ test/generate.spec.js
@@ -3,34 +3,36 @@
  * @file generate.spec.js
  */
 
-'use strict';
+ 'use strict';
 
-const chai = require('chai');
-chai.should();
-
-const generate = require('./../lib/generate');
-const tests = [{
-  file: '/tmp/mock.md',
-  id: 'mock',
-  output: '/tmp/mock.func.js',
-  retry: 3,
-  run: '.',
-  text: 'Mock',
-  type: 'title',
-  tests: {
-    test: [{
-      command: 'true',
-      describe: 'mock test',
-    }],
-  },
-}];
-
-describe('generate', () => {
-  it('should return a list of outputted files', () => {
-    const files = generate(tests);
-    files.should.be.an('Array');
-    files.should.have.lengthOf(1);
-  });
-  it('should create test files');
-  it('should create valid mocha tests');
-});
+ const chai = require('chai');
+ const os = require('os');
+ chai.should();
+ 
+ const generate = require('./../lib/generate');
+ const tests = [{
+   file: `${os.tmpdir()}/mock.md`,
+   id: 'mock',
+   output: `${os.tmpdir()}/mock.func.js`,
+   retry: 3,
+   run: '.',
+   text: 'Mock',
+   type: 'title',
+   tests: {
+     test: [{
+       command: 'true',
+       describe: 'mock test',
+     }],
+   },
+ }];
+ 
+ describe('generate', () => {
+   it('should return a list of outputted files', () => {
+     const files = generate(tests);
+     files.should.be.an('Array');
+     files.should.have.lengthOf(1);
+   });
+   it('should create test files');
+   it('should create valid mocha tests');
+ });
+ 
