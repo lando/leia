f7efd9bb2704aa101090a49a257c563926f574ba
Mike Pirog <mike@lando.dev>
2021-10-18 22:22:49 -0400

    #0: PoC of new full service Leia

--- cli/default.js
+++ cli/default.js
@@ -5,20 +5,16 @@ const chalk = require('chalk');
 class LeiaCommand extends Command {
   static id = 'leia';
   static description = 'Cleverly converts markdown files into mocha cli tests';
-  static usage = '<source> <dest> [options]';
+  static usage = '<files> <patterns> [options]';
+  static strict = false;
   static examples = [
-    'leia "examples/*.md" test',
-    'leia README.md test -p "examples/**/*.md" -p "other/RANDO.md" --retry 6 --test-header Tizzestin',
-    'leia "examples/*.md" test --split-file --output-extension funky.js',
-    'leia "*.md" --ignore BUTNOTYOU.md test --stdin',
+    'leia README.md',
+    'leia README.md "examples/**/*.md" --retry 6 --test-header Tizzestin',
+    'leia "examples/*.md" --ignore BUTNOTYOU.md test --stdin',
+    'leia README.md --shell cmd',
   ];
 
-  static args = [
-    // @NOTE: source is required but we dont set that here so we can catch downstream and
-    // print help along with the requirement message
-    {name: 'source', description: 'A file or pattern to scan for test sources'},
-    {name: 'destination', default: 'test', description: 'The output directory of the generated tests'},
-  ];
+  static args = [{name: 'tests', description: 'files or patterns to scan for test'}];
 
   static flags = {
     // Plugin commands and placeholder --debug for use with @lando/argv
@@ -48,28 +44,20 @@ class LeiaCommand extends Command {
       default: ['Clean', 'Tear', 'Burn'],
     }),
 
-    // Additional include/exclusion options
-    'pattern': flags.string({
-      char: 'p',
-      description: 'additional files or patterns to scan',
-      multiple: true,
-    }),
+    // Additional options
     'ignore': flags.string({
       char: 'i',
       description: 'files or patterns to ignore',
       multiple: true,
     }),
-
-    // Other options
-    'output-extension': flags.string({
-      char: 'o',
-      description: 'the extension of the resulting test',
-      default: 'leia.js',
-    }),
     'retry': flags.string({
       char: 'r',
       description: 'the amount of retries a failing test should get',
       default: 1,
+    }),
+    'shell': flags.string({
+      // @TODO: autodect shell and also parse into function
+      description: 'the shell to use for the tests, default is autodetected',
     }),
     'split-file': flags.boolean({
       description: 'also generate a split file for things like CircleCI',
@@ -102,19 +90,20 @@ class LeiaCommand extends Command {
     // Get modules and stuff
     // @NOTE: we do this here so we dont need to load big things like lodash just to show the CLI
     const _ = require('lodash');
-    const debug = require('debug')('leia/cli:default');
+    const debug = require('debug')('leia:cli');
     const fs = require('fs-extra');
     debug('starting default command execution');
 
-    // Grab flags and args
-    const {args, flags} = this.parse(LeiaCommand);
-
+    // Grab all teh things
+    const {args, argv, flags} = this.parse(LeiaCommand);
+    // Set args.files to argv
+    args.tests = argv;
     // If source is nill then show help and throw error
     // @NOTE: this doesnt feel like exactly right usage but it was better than the default
-    if (_.isNil(args.source)) this._help();
+    if (_.isEmpty(args.tests)) this._help();
 
-    // Combine our args and options into a parsed anc camelCase-keyed object
-    const argv = _(_.toPairs(_.merge({}, flags, args)))
+    // Combine our args and options into a parsed and camelCase-keyed object
+    const options = _(_.toPairs(_.merge({}, flags, args)))
       .map((pair) => ([_.camelCase(pair[0]), pair[1]]))
       .fromPairs()
       .value();
@@ -123,30 +112,38 @@ class LeiaCommand extends Command {
     const Leia = require('./../lib/leia');
     const leia = new Leia();
 
-    // Some advances loggin
-    debug('OPTIONS: %o', flags);
-    debug('ARGS: %o', args);
-    debug('ARGV: %o', argv);
-    debug('Ensuring directory: %s exists', argv.destination);
-    fs.mkdirpSync(argv.destination);
+    // Some advanced kenny loggins
+    debug('leia parsed args and flags into options: %o', options);
 
     // Combine all patterns and search for the things
-    const files = leia.find(_.compact(_.flatten([argv.source, argv.pattern])), argv.ignore);
-    this.log('Detected possible test source files: %s', files.join(', '));
+    const files = leia.find(options.tests, options.ignore);
+    debug('detected possible test source files: %s', files.join(', '));
 
     // Combine our args and options
-    const tests = leia.parse(files, argv);
-    this.log('Detected valid tests %s', _.map(tests, 'file').join(', '));
+    const sources = leia.parse(files, argv);
+    debug('detected valid test sources %s', _.map(sources, 'file').join(', '));
 
     // Generate test files from parsed data and return list of generated files
-    const results = leia.generate(tests);
-    this.log('Generated mocha tests to %s', results.join(', '));
+    const tests = leia.generate(sources);
+    debug('generated leia tests to %s', tests.join(', '));
 
     // Generate a split file for CCI
     if (argv.splitFile === true) {
-      fs.writeFileSync(path.join(argv.destination, 'split-file.txt'), results.join(os.EOL));
-      this.log('Writing split-file.txt to %s', argv.destination);
+      fs.writeFileSync(path.resolve('split-file.txt'), tests.join(os.EOL));
+      debug('writing split-file.txt to %s', path.resolve('split-file.txt'));
     }
+
+    // Run the actual tests
+    const Mocha = require('mocha');
+    // Instantiate a Mocha instance.
+    const mocha = new Mocha({timeout: 900000});
+    // Add all our tests
+    _.forEach(tests, (test) => mocha.addFile(test));
+
+    // Run the tests.
+    mocha.run((failures) => {
+      process.exitCode = failures ? 1 : 0;
+    });
   }
 }
 
